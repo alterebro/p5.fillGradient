418178d5912e9e9e3dd6041f77263130abeb1178
Jorge Moreno <alterebro@gmail.com>
2024-10-31 19:54:38 +0100

    build update

--- dist/p5.fillGradient.js
+++ dist/p5.fillGradient.js
@@ -1,13 +1,13 @@
-/*! p5.fillgradient v0.1.1 (c) 2022 Jorge Moreno, @alterebro */
+/*! p5.fillgradient v0.1.1 (c) 2024 Jorge Moreno, @alterebro */
 "use strict";
 
 (function() {
     "use strict";
     p5.prototype.fillGradient = function() {
-        var type = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : "linear";
-        var props = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
-        var context = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : false;
-        var _defaults = {
+        let type = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : "linear";
+        let props = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
+        let context = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : false;
+        let _defaults = {
             linear: {
                 from: [ 0, 0 ],
                 to: [ width, height ],
@@ -23,25 +23,20 @@
                 steps: [ color(255), color(0, 96, 164), color(0) ]
             }
         };
-        var _type = type.toLowerCase();
+        let _type = type.toLowerCase();
         _type = !!_defaults[_type] ? _type : "linear";
-        var _props = Object.assign(_defaults[_type], props);
-        var _ctx = !context ? canvas.getContext("2d") : context.canvas.getContext("2d");
-        var _gradients = {
-            linear: function linear() {
-                return _ctx.createLinearGradient(_props.from[0], _props.from[1], _props.to[0], _props.to[1]);
-            },
-            radial: function radial() {
-                return _ctx.createRadialGradient(_props.from[0], _props.from[1], _props.from[2], _props.to[0], _props.to[1], _props.to[2]);
-            },
-            conic: function conic() {
-                return _ctx.createConicGradient(radians(_props.from[2]), _props.from[0], _props.from[1]);
-            }
+        let _props = Object.assign(_defaults[_type], props);
+        let _ctx = !context ? canvas.getContext("2d") : context.canvas.getContext("2d");
+        let _gradients = {
+            linear: () => _ctx.createLinearGradient(_props.from[0], _props.from[1], _props.to[0], _props.to[1]),
+            radial: () => _ctx.createRadialGradient(_props.from[0], _props.from[1], _props.from[2], _props.to[0], _props.to[1], _props.to[2]),
+            conic: () => _ctx.createConicGradient(radians(_props.from[2]), _props.from[0], _props.from[1])
         };
-        var _gradient = _gradients[_type]();
-        _props.steps.forEach(function(step, i) {
-            var _step = !Array.isArray(step) ? [ step ] : step;
-            var _stop = !!_step[1] ? _step[1] : i / (_props.steps.length - 1);
+        let _gradient = _gradients[_type]();
+        _props.steps.forEach((step, i) => {
+            let _step = !Array.isArray(step) ? [ step ] : step;
+            let _stop = !!_step[1] ? _step[1] : i / (_props.steps.length - 1);
+            _stop = Math.min(1, Math.max(0, _stop));
             _gradient.addColorStop(_stop, _step[0]);
         });
         _ctx.fillStyle = _gradient;
